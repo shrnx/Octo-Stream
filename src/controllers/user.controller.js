837e269ea7bfe8dc0757bda2bac129d5847c20eb
shrnx <sharansahu1604@gmail.com>
2025-03-28 18:59:07 +0530

    added userExists check in controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,52 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
+import { ApiError } from "../utils/apiError.js"
+import { z } from "zod"
+import { User } from "../models/user.models.js"
 
 export const registerUser = asyncHandler( async(req, res) => {
+    // Get user data from frontend
+    // Validation - Not empty etc.
+    // Check if user already exists: from username and email
+    // check for images, check for avatar
+    // upload them to cloudinary, also check avatar is uploaded on cloudinary or not
+    // Create user object - create entry in DB
+    // remove password and refresh token field from response
+    // check for user creation -> return response
+
+    const { username, fullName, email, password } =  req.body;
+
+    // if(fullName === "") {                                                    We can write multiple if/elses if we want
+    //     throw new ApiError(400,  "fullName is required")
+    // }   
+
+    // if(                                                                      Here using some(), still need to write multiple if/elses
+    //     [username, fullName, email, password].some((field) => {
+    //        return field?.trim() === ""
+    //     })
+    // ) {
+    //     throw new ApiError(400, "All fields are required")
+    // }
+
+    // So the best way is using ZOD Validations
+
+
+
+
+    // Checking if the user exists? And also check that if user does not exist by email, is the username same as someone else or vice versa.
+    const userExists = User.findOne({
+        $or: [{ username }, { email }]      // checks all the values present in the object
+    })
+
+    if(userExists) {
+        throw new ApiError(409, "User with email or username exists");
+    };
+
+    
+
+})
+
+export const loginUser = asyncHandler( async(req, res) => {
     res.status(200).json({
-        message: "ok"
+        message: "chai aur code"
     })
 })
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 const router = Router()
+import { upload } from "../middlewares/multer.middleware.js"
 
-router.route("/register").post(registerUser);
-router.route("/login").post(registerUser)
+router.route("/register").post(
+    upload.fields([         // Multer Middleware, so that we can send images
+        {
+            name: "avatar",      // when we will create frontend field, then also we should name it same
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+);
+router.route("/login").post(loginUser);
 
 export default router
